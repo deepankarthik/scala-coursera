c37f35f95013ef6e5c23f3372ce7c95d74bff63a
Rengadoss <278946@cognizant.com>
2017-04-07 15:01:18 +0530

    week 1 assignment completed

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,43 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      // c!/r!(c-r)!
+      if (c == 0) 1
+      else factorial(r)/(factorial(c)*factorial(r-c))
+    }
+
+    def factorial(x: Int): Int = {
+      def factItr(value: Int, fac: Int): Int = {
+        if (value.equals(1)) fac
+        else {
+          factItr(value-1, value*fac)
+        }
+      }
+      factItr(x, 1)
+    }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+
+      def iter(chars: List[Char], acc: Int) : AnyVal= {
+        if(chars.isEmpty || acc < 0) acc
+        else if (chars.head == '(') iter(chars.tail, acc+1)
+        else if (chars.head == ')') iter(chars.tail, acc-1)
+        else iter(chars.tail, acc)
+      }
+      iter(chars, 0) == 0
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money < 0 || coins.isEmpty) 0
+      else if (money <= 0 && !coins.isEmpty) 0
+      else countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
   }
